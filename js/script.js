9cb49e89a045a9adfdd4eb0cd7b7a8733d38e01e
Alvaro Iriondo <alvaropereyra11@hotmail.com>
2024-08-29 18:00:02 -0300

    Script js
    Consola, talle, entradas y funcion faltante

--- js/script.js
+++ js/script.js
@@ -1,41 +1,51 @@
-let terminos = confirm("¿Estas de acuerdo con nuestros terminos y condiciones?");
 const precios = [50, 75, 100, 350, 500];
-const entradas =['Trainee', 'Junior', 'Senior','Manager','CEO'];
+const entradas = ["Trainee", "Junior", "Senior", "Manager", "CEO"];
+let terminos = confirm(
+    "¿Estas de acuerdo con nuestros terminos y condiciones?"
+  );
 let trainee = precios[0];
 let junior = precios[1];
 let senior = precios[2];
 let manager = precios[3];
 let ceo = precios[4];
-
-for (let i=0; i < entradas.length; i++) {
-    console.log('El precio de la entrada ' + entradas[i] + ' es ' + precios[i])
+let nombre = "";
+let numero = 0;
+function datosUsuario() {
+  nombre = prompt("Ingresa tu nombre");
+  numero = prompt("Ingresa tu numero de telefono");
 }
-function mostrarPrecio() {
-  let Eleccion = prompt(
-    "Elige un tipo de entradas: trainee, junior, senior, manager o ceo");
-  switch (Eleccion) {
-    case "trainee":
-      alert(
-        "Gracias por elegir la entrada Trainee. El precio total es " + trainee);
+datosUsuario();
+for (let i = 0; i < entradas.length; i++) {
+  console.log(nombre[0].toUpperCase() + nombre.substring(1) + " el precio de la entrada " + entradas[i] + " es " + precios[i] + ".");
+}
+function elegirEntrada(eleccion) {
+    function talleCamiseta() {
+        return prompt('¿Que talle de camiseta prefieres? (S, M, L, XL, XXL)')
+    }
+    let talle; 
+  switch (eleccion) {
+    case trainee:
+      console.log(
+        'Gracias por elegir la entrada Trainee. Bienvenido a CoderFest. Te contactaremos al ' + numero);
       break;
-    case "junior":
-      alert(
-        "Gracias por elegir la entrada Junior. El precio total es " + junior);
+    case junior:
+      console.log(
+        'Gracias por elegir la entrada Junior. Bienvenido a CoderFest. Te contactaremos al ' + numero );
       break;
-    case "senior":
-      alert(
-        "Gracias por elegir la entrada Senior. El precio total es " + senior);
+    case senior:
+        talle =  talleCamiseta();
+      console.log('Gracias por elegir la entrada Senior. Bienvenido a CoderFest. Te contactaremos al ' + numero + ' y te enviaremos la camiseta talle ' + talle + '.');
       break;
-    case "manager":
-      alert(
-        "Gracias por elegir la entrada Manager. El precio total es " + manager);
-        break;
-    case "ceo":
-      alert("Gracias por elegir la entrada CEO. El precio total es " + ceo);
+    case manager:
+        talle = talleCamiseta();
+      console.log('Gracias por elegir la entrada Manager. Bienvenido a CoderFest. Te contactaremos al ' + numero + ' y te enviaremos la camiseta talle ' + talle + '.');
+      break;
+    case ceo:
+        talle = talleCamiseta();
+      console.log('Gracias por elegir la entrada CEO. Bienvenido a CoderFest. Te contactaremos al ' + numero + ' y te enviaremos la camiseta talle ' + talle + '.');
       break;
     default:
-      alert("Debes elegir una de las entradas disponibles.");
+      console.log('Debes elegir una de las entradas disponibles.');
       break;
   }
 }
-mostrarPrecio();
