3797529917a671f129013b851affb44c79b7e253
Alvaro Iriondo <alvaropereyra11@hotmail.com>
2024-10-10 18:48:01 -0300

    Proyecto final

--- js/script.js
+++ js/script.js
@@ -1,31 +1,48 @@
-const entradas = [
-  { id: 1, nombre: "Trainee", beneficios: "Acceso general", precio: 50 },
-  {
-    id: 2,
-    nombre: "Junior",
-    beneficios: "Acceso general + 1 bebida gratis",
-    precio: 100,
-  },
-  {
-    id: 3,
-    nombre: "Senior",
-    beneficios: "Acceso general + 2 bebidas gratis + Camiseta CoderFest",
-    precio: 150,
-  },
-  {
-    id: 4,
-    nombre: "Manager",
-    beneficios: "Acceso VIP + 2 bebidas gratis + Camiseta CoderFest",
-    precio: 350,
-  },
-  {
-    id: 5,
-    nombre: "CEO",
-    beneficios:
-      "Acceso VIP + Bebidas ilimitadas + Camiseta CoderFest + Meet & Greet con artistas invitados",
-    precio: 500,
-  },
-];
+function crearEntrada(entradas) {
+  const entradaContainer = document.getElementById("entrada-container");
+  entradas.forEach((entrada) => {
+    const nuevaEntrada = document.createElement("div");
+    nuevaEntrada.classList = "entrada-producto";
+    nuevaEntrada.innerHTML = `
+      <img src="./img/${entrada.id}.jpg">
+      <h2>${entrada.nombre}</h2>
+      <p>${entrada.beneficios}</p>
+      <span>$${entrada.precio}</span>
+      <button class="btn-comprar">Comprar</button>
+    `;
+    nuevaEntrada.querySelector(".btn-comprar").addEventListener("click", () => {
+      if (entrada.id === 4) {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "Entrada agotada",
+        });
+      } else {
+        const productoEnCarrito = carrito.find(
+          (producto) => producto.id === entrada.id
+        );
+        if (productoEnCarrito) {
+          productoEnCarrito.cantidad++;
+        } else {
+          carrito.push({ ...entrada, cantidad: 1 });
+        }
+        renderizarCarrito();
+      }
+    });
+    entradaContainer.appendChild(nuevaEntrada);
+  });
+}
+
+fetch("./js/entradas.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Error al cargar el archivo JSON");
+    }
+    return response.json();
+  })
+  .then((informacion) => {
+    crearEntrada(informacion);
+  });
 
 let carrito = [];
 
@@ -45,7 +62,7 @@ function renderizarCarrito() {
   const carritoContainer = document.getElementById("carrito");
   carritoContainer.innerHTML = "";
   if (carrito.length === 0) {
-    carritoContainer.innerHTML = '<p>Carrito vacío</p>';
+    carritoContainer.innerHTML = "<p>Carrito vacío</p>";
   } else {
     carrito.forEach((producto, index) => {
       const li = document.createElement("li");
@@ -62,7 +79,7 @@ function renderizarCarrito() {
   }
   actualizarTotal();
   guardarCarrito();
-  actualizarVisibilidadTotalCarrito(); 
+  actualizarVisibilidadTotalCarrito();
 }
 
 function incrementarCantidad(index) {
@@ -93,37 +110,47 @@ function actualizarTotal() {
 }
 
 document.getElementById("vaciar-carrito").addEventListener("click", () => {
-  carrito = [];
-  renderizarCarrito();
+  Swal.fire({
+    title: '¿Estás seguro?',
+    text: "Se borrara todo el contenido del carro.",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Sí, vaciar carro.',
+    cancelButtonText: 'Cancelar'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      carrito = [];
+      renderizarCarrito();
+      Swal.fire(
+        '¡Vaciado!',
+        'El carrito ha sido vaciado.',
+        'success'
+      );
+    }
+  });
 });
+
 document.getElementById("finalizar-compra").addEventListener("click", () => {
   document.getElementById("formulario-compra").style.display = "block";
 });
 function actualizarVisibilidadTotalCarrito() {
   const totalCarrito = document.getElementById("total-carrito");
   if (carrito.length === 0) {
-    totalCarrito.style.display = 'none';
+    totalCarrito.style.display = "none";
   } else {
-    totalCarrito.style.display = 'block';
+    totalCarrito.style.display = "block";
   }
 }
 
-
-document.getElementById('formulario-compra').addEventListener('submit', function(evento) {
+document
+  .getElementById("formulario-compra")
+  .addEventListener("submit", function (evento) {
     evento.preventDefault();
-    const nombre = document.getElementById('nombre').value;
-    const direccion = document.getElementById('direccion').value;
-    mostrarResumen(nombre, direccion);
-});
-
-function mostrarResumen(nombre, direccion) {
-    document.querySelector('.resumen').innerHTML = `
-        <h2>Resumen de compra</h2>
-        <p>Nombre: ${nombre}</p>
-        <p>Dirección: ${direccion}</p>
-    `;
-}
-
+    const nombre = document.getElementById("nombre").value;
+    const direccion = document.getElementById("direccion").value;
+  });
 
 document.getElementById("carrito-imagen").addEventListener("click", () => {
   const carritoContenido = document.getElementById("carrito-contenido");
@@ -132,71 +159,42 @@ document.getElementById("carrito-imagen").addEventListener("click", () => {
 
 cargarCarrito();
 
-function crearEntrada(entradas) {
-  const entradaContainer = document.getElementById("entrada-container");
-  entradas.forEach((entrada) => {
-    const nuevaEntrada = document.createElement("div");
-    nuevaEntrada.classList = "entrada-producto";
-    nuevaEntrada.innerHTML = `
-      <img src="./img/${entrada.id}.jpg">
-      <h2>${entrada.nombre}</h2>
-      <p>${entrada.beneficios}</p>
-      <span>$${entrada.precio}</span>
-      <button class="btn-comprar">Comprar</button>
-    `;
-    nuevaEntrada.querySelector(".btn-comprar").addEventListener("click", () => {
-      const productoEnCarrito = carrito.find(
-        (producto) => producto.id === entrada.id
-      );
-      if (productoEnCarrito) {
-        productoEnCarrito.cantidad++;
-      } else {
-        carrito.push({ ...entrada, cantidad: 1 });
-      }
+document
+  .getElementById("formulario-compra")
+  .addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const pais = document.getElementById("pais").value;
+    const ciudad = document.getElementById("ciudad").value;
+    const direccion = document.getElementById("direccion").value;
+
+    Swal.fire({
+      icon: "success",
+      title: "Compra finalizada",
+      text: "Gracias por tu compra!",
+    }).then(() => {
+      carrito = [];
       renderizarCarrito();
     });
-    entradaContainer.appendChild(nuevaEntrada);
+
+    modal.style.display = "none";
   });
-}
-
-crearEntrada(entradas);
-
-document.getElementById('formulario-compra').addEventListener('submit', function(evento) {
-  evento.preventDefault();
-  const nombre = document.getElementById('nombre').value;
-  const apellido = document.getElementById('apellido').value;
-  const pais = document.getElementById('pais').value;
-  const ciudad = document.getElementById('ciudad').value;
-  const direccion = document.getElementById('direccion').value;
-  mostrarResumen(nombre, apellido, pais, ciudad, direccion);
-  modal.style.display = "none"; 
-});
-
-function mostrarResumen(nombre, apellido, pais, ciudad, direccion) {
-  document.querySelector('.resumen').innerHTML = `
-      <h2>Resumen de compra</h2>
-      <p>Nombre: ${nombre}</p>
-      <p>Apellido: ${apellido}</p>
-      <p>País: ${pais}</p>
-      <p>Ciudad: ${ciudad}</p>
-      <p>Dirección: ${direccion}</p>
-  `;
-}
 
 const modal = document.getElementById("modal");
 const btnFinalizarCompra = document.getElementById("finalizar-compra");
 const spanCerrar = document.getElementsByClassName("cerrar")[0];
 
-btnFinalizarCompra.onclick = function() {
-modal.style.display = "block";
-}
+btnFinalizarCompra.onclick = function () {
+  modal.style.display = "block";
+};
 
-spanCerrar.onclick = function() {
-modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-if (event.target == modal) {
+spanCerrar.onclick = function () {
   modal.style.display = "none";
-}
-}
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
